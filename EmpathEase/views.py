7e4cc77b99c25e7f71d40726f65a50a27d173661
cczapatag <69641329+cczapatag@users.noreply.github.com>
2023-09-26 21:36:38 -0500

    Contexto aplicado

--- EmpathEase/views.py
+++ EmpathEase/views.py
@@ -5,19 +5,34 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from .models import Chat
 from django.utils import timezone
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 
-
-openaiApiKey = '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'
+openaiApiKey = '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'
 openai.api_key = openaiApiKey
 
+def summarize_messages(messages):
+    summarizer = LsaSummarizer()
+    parser = PlaintextParser.from_string(messages, Tokenizer("english"))
+    summary = summarizer(parser.document, 1)  # Resumir a 1 oración
+    return str(summary[0])
 
-def askOpenai(message):
+def remove_redundant_messages(messages):
+    cleaned_messages = []
+    previous_message = None
+
+    for message in messages:
+        if message["content"] != previous_message:
+            cleaned_messages.append(message)
+            previous_message = message["content"]
+
+    return cleaned_messages
+
+def askOpenai(messages):
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are an helpful assistant."},
-            {"role": "user", "content": message},
-        ]
+        model="gpt-3.5-turbo",
+        messages=messages,
     )
 
     answer = response.choices[0].message.content.strip()
@@ -25,14 +40,33 @@ def askOpenai(message):
 
 # Create your views here.
 def empathEase(request):
-    chats = Chat.objects.filter(user=request.user)
+    user_id = request.user.id
+    chats = Chat.objects.filter(user_id=user_id)
+    
+    messages = [
+        {"role": "system", "content": "You are a virtual psychologist here to listen and provide support."}
+    ]
+    for chat in chats:
+        messages.append({"role": "user", "content": chat.message})
+        messages.append({"role": "assistant", "content": chat.response})
+
+    messages = remove_redundant_messages(messages)  # Elimina mensajes redundantes
+    messages_text = "\n".join([message["content"] for message in messages])
+    summarized_messages = summarize_messages(messages_text)  # Resume los mensajes
+    
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = askOpenai(message)
-        chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+        messages.append({"role": "user", "content": message})
+        
+        response = askOpenai(messages)
+        
+        chat = Chat(user_id=user_id, message=message, response=response, created_at=timezone.now())
         chat.save()
-        return JsonResponse({'message': message,'response': response})
-    return render(request, 'empathEase.html', {'chats': chats})
+        
+        return JsonResponse({'message': message, 'response': response})
+    
+    return render(request, 'empathEase.html', {'chats': chats, 'summarized_messages': summarized_messages})
+
 
 def login(request):
     if request.method == 'POST':
